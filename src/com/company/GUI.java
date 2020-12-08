4aecb828b758316a18d137f7c0da60063af16c5e
szabolcsdanielnagy <61884578+szabolcsdanielnagy@users.noreply.github.com>
2020-12-08 11:04:28 +0000

    GUI and generator already works. Generated passwords tab not yet working, code sorting still required.

--- src/com/company/GUI.java
+++ src/com/company/GUI.java
@@ -6,43 +6,61 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GUI implements ActionListener {
-    private JFrame frame;
-    private JPanel panel;
-    private JButton generateButton;
-    private JLabel label;
-    private JCheckBox containLowerCase, containUpperCase, containSymbols;
-    private JSlider passwordLength;
+    private final JFrame frame;
+    private final JTabbedPane tabbedPane;
+    private final JPanel passwordGeneratorTab, generatedPasswordsTab;
+    private final JButton generateButton;
+    private final JLabel preferencesLabel;
+    private final JCheckBox useLower, useUpper, useSymbols, useNumeric;
+    private final JSlider passwordLength;
+    private final PasswordGenerator passwordGenerator;
 
     public GUI() {
-        label = new JLabel("Preferences");
+        tabbedPane = new JTabbedPane();
+        preferencesLabel = new JLabel("Preferences");
         generateButton = new JButton("Generate password");
-        passwordLength = new JSlider(1,25);
+        passwordLength = new JSlider(1, 25);
         setUpButton();
-        panel = new JPanel();
-        setUpPanel();
+        passwordGeneratorTab = new JPanel();
+        generatedPasswordsTab = new JPanel();
+        setUpPasswordGeneratorTab();
+        setUpGeneratedPasswordsTab();
         frame = new JFrame();
         setUpFrame();
-        containLowerCase = new JCheckBox("Lower case");
-        containUpperCase = new JCheckBox("Upper case");
-        containSymbols = new JCheckBox("Contain symbols");
+        useLower = new JCheckBox("Lower case");
+        useUpper = new JCheckBox("Upper case");
+        useSymbols = new JCheckBox("Contain symbols");
+        useNumeric = new JCheckBox("Contain numeric");
         addCheckBoxes();
         setUpSlider();
+        passwordGenerator = new PasswordGenerator();
+        setUpTabbedPane();
     }
 
     private void setUpButton() {
         generateButton.addActionListener(this);
     }
 
-    private void setUpPanel() {
-        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-        panel.setLayout(new GridLayout(0,1));
-        panel.add(generateButton);
-        panel.add(label);
+    private void setUpTabbedPane() {
+        tabbedPane.addTab("Password generator", passwordGeneratorTab);
+        tabbedPane.addTab("Generated passwords", generatedPasswordsTab);
+    }
+
+    private void setUpPasswordGeneratorTab() {
+        passwordGeneratorTab.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        passwordGeneratorTab.setLayout(new GridLayout(0, 1));
+        passwordGeneratorTab.add(generateButton);
+        passwordGeneratorTab.add(preferencesLabel);
+    }
+
+    private void setUpGeneratedPasswordsTab() {
+        generatedPasswordsTab.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        generatedPasswordsTab.setLayout(new GridLayout(0, 1));
     }
 
     private void setUpFrame() {
         frame.setPreferredSize(new Dimension(400, 300));
-        frame.add(panel,BorderLayout.CENTER );
+        frame.add(tabbedPane, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Password Generator");
         frame.pack();
@@ -51,12 +69,14 @@ public class GUI implements ActionListener {
     }
 
     private void addCheckBoxes() {
-        panel.add(containLowerCase);
-        panel.setVisible(true);
-        panel.add(containUpperCase);
-        panel.setVisible(true);
-        panel.add(containSymbols);
-        panel.setVisible(true);
+        passwordGeneratorTab.add(useLower);
+        passwordGeneratorTab.setVisible(true);
+        passwordGeneratorTab.add(useUpper);
+        passwordGeneratorTab.setVisible(true);
+        passwordGeneratorTab.add(useSymbols);
+        passwordGeneratorTab.setVisible(true);
+        passwordGeneratorTab.add(useNumeric);
+        passwordGeneratorTab.setVisible(true);
     }
 
     private void setUpSlider() {
@@ -64,11 +84,12 @@ public class GUI implements ActionListener {
         speedPanel.setLayout(new GridLayout(2, 1));
         speedPanel.add(new JLabel("Password length", SwingConstants.CENTER));
         speedPanel.add(passwordLength);
-        panel.add(speedPanel, BorderLayout.NORTH);
+        passwordGeneratorTab.add(speedPanel, BorderLayout.NORTH);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        String password = passwordGenerator.generatePassword(passwordLength.getValue(), useLower.isSelected(), useUpper.isSelected(), useSymbols.isSelected(), useNumeric.isSelected());
+        JOptionPane.showMessageDialog(frame,password,"Generated password", JOptionPane.INFORMATION_MESSAGE);
     }
 }
--- src/com/company/PasswordGenerator.java
+++ src/com/company/PasswordGenerator.java
@@ -1,4 +1,59 @@
 package com.company;
 
+import java.security.SecureRandom;
+import java.util.ArrayList;
+
 public class PasswordGenerator {
+
+    private final String LOWER = "abcdefghijklmnopqrstuvwxyz";
+    private final String UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private final String NUMERIC = "0123456789";
+    private final String SYMBOLS = "!@#$%^&*_=+-/";
+    private SecureRandom random;
+    private boolean useLower;
+    private boolean useUpper;
+    private boolean useNumeric;
+    private boolean useSymbols;
+    private ArrayList<String> usages;
+
+    public PasswordGenerator() {
+        random = new SecureRandom();
+        usages = new ArrayList<>();
+        useLower = false;
+        useUpper = false;
+        useNumeric = false;
+        useSymbols = false;
+    }
+
+    public String generatePassword(int length, boolean useLower, boolean useUpper,
+                                   boolean useSymbols, boolean useNumeric) {
+        this.useLower = useLower;
+        this.useUpper = useUpper;
+        this.useSymbols = useSymbols;
+        this.useNumeric = useNumeric;
+        setUpUsages();
+        StringBuilder password = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            String charCategory = usages.get(random.nextInt(usages.size()));
+            int position = random.nextInt(charCategory.length());
+            password.append(charCategory.charAt(position));
+        }
+        usages = new ArrayList<>();
+        return password.toString();
+    }
+
+    public void setUpUsages() {
+        if (useLower) {
+            usages.add(LOWER);
+        }
+        if (useNumeric) {
+            usages.add(NUMERIC);
+        }
+        if (useSymbols) {
+            usages.add(SYMBOLS);
+        }
+        if (useUpper) {
+            usages.add(UPPER);
+        }
+    }
 }
